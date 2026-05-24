88bb353f78339f6a748ce3f398d2a87b38b74fbe
Charles Eromose <okuekhamhene@gmail.com>
2026-05-24 05:49:52 +0100

    created the layout and app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-// import { redirect } from 'next/navigation';
-// import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 
-// export default function RootPage() {
-//   const token = cookies().get('wn_token');
-//   if (token) redirect('/inbox');
-//   redirect('/login');
-// }
+export default function RootPage() {
+  const token = cookies().get('wn_token');
+  if (token) redirect('/inbox');
+  redirect('/login');
+}
